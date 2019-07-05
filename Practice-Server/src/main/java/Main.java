90106d0045a4930ca537dd6d7f1216ab8458f41d
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2019-07-05 13:34:18 +0200

    added to Practice projects and pom

--- Practice-Server/src/main/java/Main.java
+++ Practice-Server/src/main/java/Main.java
@@ -1,9 +1,36 @@
-package src.main.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+        boolean client = false;
+        int port = 5000;
+        try  (ServerSocket serverSocket = new ServerSocket(port)){
+            System.out.println("Server Created on port:" + port);
+            Socket socket = serverSocket.accept();
+            System.out.println("Client Connected");
+            BufferedReader input = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+            client = true;
+
+            while(client){
+                String echoString = input.readLine();
+                if (echoString.equals("exit")){
+                    client = false;
+                }
+                output.println("Echo from server " + echoString);
+            }
+
+        } catch (IOException e){
+            System.out.println("Server Exception "+ e.getLocalizedMessage());
+        }
     }
 
 }
