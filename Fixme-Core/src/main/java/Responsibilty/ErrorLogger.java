1728fcf69e3489b1748ef7ba6a5ce3c82b453373
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2019-07-05 12:22:43 +0200

    added Loggers to core and a practice module

---
+++ Fixme-Core/src/main/java/Responsibilty/ErrorLogger.java
@@ -0,0 +1,13 @@
+package src.main.java.Responsibilty;
+
+public class ErrorLogger extends AbstractLogger {
+
+    public ErrorLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Error Console::Logger: " + message);
+    }
+}
